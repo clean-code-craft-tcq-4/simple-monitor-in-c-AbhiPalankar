25ddcad34fb9142b67ec57b1082e03ab8317ed35
AbhiPalankar <105204004+AbhiPalankar@users.noreply.github.com>
2022-07-12 21:15:54 +0530

    Update Batterystatus_UT.h
    added function declaration

--- Batterystatus_UT.h
+++ Batterystatus_UT.h
@@ -3,7 +3,8 @@
 
 #include <assert.h>
 
-
+extern int batteryIsOk( float soc, float temp , float chargeRate);
+extern int batteryIsNotOk( float soc, float temp , float chargeRate);
 extern int CheckbatteryIsOk( float expectedsoc, float expectedTemp , float expectedChargeRate);
 extern int CheckbatteryIsNotOk( float expectedsoc, float expectedTemp , float expectedChargeRate);
 extern void CheckBatteryStatus(void);
