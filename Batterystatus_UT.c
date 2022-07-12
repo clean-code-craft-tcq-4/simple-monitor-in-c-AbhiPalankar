457d1f35f37c17c063374a787465ff14c809f3f1
AbhiPalankar <105204004+AbhiPalankar@users.noreply.github.com>
2022-07-12 23:13:56 +0530

    Update Batterystatus_UT.c
    added unit

--- Batterystatus_UT.c
+++ Batterystatus_UT.c
@@ -1,18 +1,18 @@
 #include "Batterystatus_UT.h"
 
 
-int CheckbatteryIsOk(float expectedsoc, float expectedTemp , float expectedChargeRate)
+int CheckbatteryIsOk(float expectedsoc, float expectedTemp , float expectedChargeRate, char expectedTempUnit)
 {	
-	batteryIsOk(expectedsoc,expectedTemp,expectedChargeRate);
+	batteryIsOk(expectedsoc,expectedTemp,expectedChargeRate,expectedTempUnit);
 }
-int CheckbatteryIsNotOk(float expectedsoc, float expectedTemp , float expectedChargeRate)
+int CheckbatteryIsNotOk(float expectedsoc, float expectedTemp , float expectedChargeRate, char expectedTempUnit)
 {	
-	batteryIsNotOk(expectedsoc,expectedTemp,expectedChargeRate);
+	batteryIsNotOk(expectedsoc,expectedTemp,expectedChargeRate,expectedTempUnit);
 }
 
 void CheckBatteryStatus(void)
 {
-  assert(CheckbatteryIsOk(25, 70, 0.7));
-  assert(CheckbatteryIsNotOk(50, 85, 0));
+  assert(CheckbatteryIsOk(25, 70, 0.7,'C'));
+  assert(CheckbatteryIsNotOk(50, 85, 0,'F'));
 
 }
