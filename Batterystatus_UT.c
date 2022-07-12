b85e6b18bd22e4b7f53b472bfb4c103bd1c2fdb4
AbhiPalankar <105204004+AbhiPalankar@users.noreply.github.com>
2022-07-12 23:34:07 +0530

    Update Batterystatus_UT.c
    extend

--- Batterystatus_UT.c
+++ Batterystatus_UT.c
@@ -13,6 +13,8 @@ int CheckbatteryIsNotOk(float expectedsoc, float expectedTemp , float expectedCh
 void CheckBatteryStatus(void)
 {
   assert(CheckbatteryIsOk(25, 70, 0.7,'C'));
+  assert(CheckbatteryIsOk(25, 70, 0.7,'F'));
+  assert(CheckbatteryIsNotOk(50, 85, 0,'C'));
   assert(CheckbatteryIsNotOk(50, 85, 0,'F'));
 
 }
