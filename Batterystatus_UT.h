754a283d67dd563bad23bbb14b893e9d22d17143
AbhiPalankar <105204004+AbhiPalankar@users.noreply.github.com>
2022-07-12 23:15:13 +0530

    Update Batterystatus_UT.h
    added unit

--- Batterystatus_UT.h
+++ Batterystatus_UT.h
@@ -5,8 +5,8 @@
 #include "ev_bms.h"
 
 
-extern int CheckbatteryIsOk( float expectedsoc, float expectedTemp , float expectedChargeRate);
-extern int CheckbatteryIsNotOk( float expectedsoc, float expectedTemp , float expectedChargeRate);
+extern int CheckbatteryIsOk( float expectedsoc, float expectedTemp , float expectedChargeRate, char expectedTempUnit);
+extern int CheckbatteryIsNotOk( float expectedsoc, float expectedTemp , float expectedChargeRate, char expectedTempUnit);
 extern void CheckBatteryStatus(void);
 
 
