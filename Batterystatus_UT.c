2d7b3ce5a4ced7ca461a1a9efee28cd14cd56a1e
AbhiPalankar <105204004+AbhiPalankar@users.noreply.github.com>
2022-07-12 21:14:44 +0530

    Update Batterystatus_UT.c
    added battery health check function

--- Batterystatus_UT.c
+++ Batterystatus_UT.c
@@ -1,5 +1,21 @@
 #include "Batterystatus_UT.h"
 
+int batteryIsOk( float soc, float temp , float chargeRate)
+{	
+	float stateOfCharge = inttBatterySocIsOk(soc);
+	float temperature = inttBatteryTempIsOk(temp);
+	float chargerate = inttBatteryChargeRateIsOk(chargeRate);
+	return (stateOfCharge && temperature && chargerate);
+}
+
+int batteryIsNotOk(float soc, float temp , float chargeRate)
+{	
+	float stateOfCharge = inttBatterySocIsOk(soc);
+	float temperature = inttBatteryTempIsOk(temp);
+	float chargerate = inttBatteryChargeRateIsOk(chargeRate);
+	return (stateOfCharge || temperature || chargerate);
+}
+
 int CheckbatteryIsOk(float expectedsoc, float expectedTemp , float expectedChargeRate)
 {	
 	batteryIsOk(expectedsoc,expectedTemp,expectedChargeRate);
